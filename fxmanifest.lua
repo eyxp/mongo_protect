7e2a10baa791715f17e1ce33c4f3ceb5ca2a516c
Jonassシ <73247288+MongodbClient@users.noreply.github.com>
2023-09-30 10:54:13 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'adamant'
 game 'gta5'
-version '1.0.2-RE22LEAS222222E12222222222222'
+version '1.0.2-RE22LEAS222222E1222222222222222'
 author '💗 Jonas'
 description 'Protect your Server from dumpers!'
 
