4c1736eae9f988f2271a58ecf3176886899cf5d5
Jonas シ <73247288+eyxp@users.noreply.github.com>
2024-09-17 08:54:49 +0200

    feat(lang): implement 'en'

--- src/locales/mongo_en.lua
+++ src/locales/mongo_en.lua
@@ -1,5 +1,10 @@
 Locales = Locales or {};
-
 Locales['en'] = {
-    
+    ['stopped_own_resource'] = 'The protector has been successfully stopped!',
+    ['script_added'] = '%s | %s was successfully added!',
+    ['resource_cache_clear'] = 'The resource %s has been cleared from the cache!',
+    ['script_not_found'] = '%s | The script was not added correctly!',
+    ['file_not_found'] = '%s | %s was not found!',
+    ['file_overload'] = '%s | %s | An attempt was made to load the code repeatedly!',
+    ['sended_code'] = '%s | %s | %s client-side code has been sent!'
 }
